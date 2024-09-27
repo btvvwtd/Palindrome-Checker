a6e81777f62b923f434a835e1f42f0e34ca551d3
Artur <arturdmytryshyn@gmail.com>
2024-09-27 20:08:24 +0200

    second

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('check-btn').addEventListener('click',function(){
+    const inputText = document.getElementById('text-input').value;
+    if (!inputText) {
+        alert("Please input a value");
+        return;
+    }
+
+    const cleanedText = inputText.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    const reversedText = cleanedText.split('').reverse().join('');
+
+    const resultElement = document.getElementById('result');
+
+    if (cleanedText === reversedText) {
+        resultElement.textContent = `${inputText} is a palindrome`;
+    } else {
+        resultElement.textContent = `${inputText} is not a palindrome`;
+    }
+});
